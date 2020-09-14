2fc81e711f34513b3176146cfe546a137753f63e
Saurav Shah <sauravhshah99@gmail.com>
2020-09-14 15:07:48 +0545

    Add project files.

---
+++ SalesTransaction.Application.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesTransaction.Application.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
